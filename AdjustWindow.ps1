d63aa141dc8eaee6959d25ef547126bb801b7c55
Michael Exel <micha.exel@googlemail.com>
2022-09-15 08:02:29 +0200

    Anpassungen

---
+++ AdjustWindow.ps1
@@ -0,0 +1,36 @@
+﻿function AdjustWindow {
+
+#[cmdletbinding(DefaultParameterSetName='Name')]
+
+Param (
+    [parameter(Mandatory=$False,
+        ValueFromPipelineByPropertyName=$True, ParameterSetName='Name')]
+    [string]$ProcessName='*',
+    [parameter(Mandatory=$True,
+        ValueFromPipeline=$False,              ParameterSetName='Id')]
+    [int]$Id,
+    [int]$X,
+    [int]$Y,
+    [int]$Width,
+    [int]$Height
+)
+BEGIN{
+    echo "Get-Process"
+    $PSF =  ($env:PS_FUNCTIONS)
+    . $PSF/Move_Window.ps1
+    #$ProcessName = (Get-Process | Where-Object {$_.MainWindowTitle -like "Raid: Shadow Legends*"}) | Select-Object Name    
+    Set-Window -ProcessName $ProcessName -X $X -Y $Y -Width $Width -Height $Height -Passthru
+}
+
+}
+
+function Install-Software { Write-Host 'I installed some software. Yippee!' }
+
+
+
+#AdjustWindow -ProcessName Raid -X 1600 -Y 10 -Width 1123 -Height 870
+
+
+# Angepasst ans RSL Fenster
+#AdjustWindow -ProcessName Raid -X 0 -Y 0 -Width 1107 -Height 635
+
---
+++ test/testAdjustWindow.ps1
@@ -0,0 +1,10 @@
+$PSF =  ($env:PS_FUNCTIONS)
+. $PSF/AdjustWindow.ps1
+
+# AdjustWindow -ProcessName Raid -X 1600 -Y 10 -Width 2720 -Height 1139
+
+# Angepasst ans RSL Fenster
+AdjustWindow -ProcessName Raid -X 0 -Y 0 -Width 1107 -Height 635
+
+# Clan vs Clan
+# AdjustWindow -ProcessName Raid -X 0 -Y 0 -Width 1107 -Height 900
