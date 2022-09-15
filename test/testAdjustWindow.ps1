d5965d2f0894cdf1f16bac0ced5d02f2b08bf4ef
Michael Exel <micha.exel@googlemail.com>
2022-09-15 17:10:13 +0200

    Anpassungen

--- test/testAdjustWindow.ps1
+++ test/testAdjustWindow.ps1
@@ -9,5 +9,5 @@ AdjustWindow -ProcessName Raid -X 0 -Y 0 -Width 1107 -Height 635
 # Clan vs Clan
 # AdjustWindow -ProcessName Raid -X 0 -Y 0 -Width 1107 -Height 900
 
-# kompaktr
+# kompakt
 AdjustWindow -ProcessName Raid -X 0 -Y 0 -Width 1280 -Height 720
