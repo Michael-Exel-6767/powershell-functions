9754279a650dc948738e27dc95e804649c1e570d
Michael Exel <micha.exel@gmail.com>
2023-04-23 09:38:10 +0200

    Find Window Under Cursor

---
+++ FindWindowUnderCursor.ps1
@@ -0,0 +1,59 @@
+﻿<#
+see 
+   https://github.com/MScholtes/PSVirtualDesktop
+   https://stackoverflow.com/questions/27839668/get-window-title-via-mouse-cursor-in-powershell-wasp
+#>
+
+Add-Type -TypeDefinition @"
+using System; 
+using System.Runtime.InteropServices;
+public class Utils
+{ 
+    public struct RECT
+    {
+        public int Left;        
+        public int Top;         
+        public int Right;       
+        public int Bottom;     
+    }
+
+    [DllImport("user32.dll")]
+    public static extern bool GetWindowRect(
+        HandleRef hWnd,
+        out RECT lpRect);
+}
+"@
+
+#$OldDesktop = Get-CurrentDesktop
+#"Number of virtual desktops: $(Get-DesktopCount)"
+#"Name of current desktop: $(Get-DesktopName($OldDesktop))"
+
+Add-Type -AssemblyName System.Windows.Forms
+$p = [Windows.Forms.Cursor]::Position
+Get-Process | %{ 
+  #  [int32[]]$handles
+    if ($_.MainWindowHandle)
+    {
+        $o = New-Object -TypeName System.Object            
+        $href = New-Object -TypeName System.RunTime.InteropServices.HandleRef -ArgumentList $o, $_.MainWindowHandle            
+
+        $rect = New-Object utils+RECT            
+        [Void][Utils]::GetWindowRect($href, [ref]$rect)
+
+        if ($p.X -ge $rect.Left -and $p.X -le $rect.Right -and 
+            $p.Y -ge $rect.Top -and $p.Y -le $rect.Bottom
+           )
+        {
+            try {
+                $title="$_.MainWindowTitle"
+                if ((Get-DesktopFromWindow ($_.MainWindowHandle)).IsVisible)
+                {
+                    $_.MainWindowTitle + " - " + $_.Handle
+                }
+            }
+            catch {}
+           
+        }
+    }
+
+}
--- test/testAdjustWindow.ps1
+++ test/testAdjustWindow.ps1
@@ -4,10 +4,10 @@ $PSF =  ($env:PS_FUNCTIONS)
 # AdjustWindow -ProcessName Raid -X 1600 -Y 10 -Width 2720 -Height 1139
 
 # Angepasst ans RSL Fenster
-AdjustWindow -ProcessName Raid -X 0 -Y 0 -Width 1107 -Height 635
+#AdjustWindow -ProcessName Raid -X 0 -Y 0 -Width 1107 -Height 635
 
 # Clan vs Clan
 # AdjustWindow -ProcessName Raid -X 0 -Y 0 -Width 1107 -Height 900
 
 # kompakt
-AdjustWindow -ProcessName Raid -X 0 -Y 0 -Width 1280 -Height 720
+AdjustWindow -ProcessName HD-Player -X 0 -Y 0 -Width 1280 -Height 720
